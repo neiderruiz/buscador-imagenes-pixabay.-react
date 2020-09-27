c13c8e133a9593304978512d6977240a7806ea30
neiderbtc <neiderbtc@gmail.com>
2020-09-26 23:25:48 -0400

    edicion index y renombrado docs

---
+++ docs/precache-manifest.d3b5839f4242a086ce70c24fa9b3a50f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc80d4f54adc4405f1082dd238f238ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49c2a76b74b481e974cd",
+    "url": "/static/css/main.50579e86.chunk.css"
+  },
+  {
+    "revision": "6c9371e9bc61c9e7245b",
+    "url": "/static/js/2.e3bfa668.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3bfa668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49c2a76b74b481e974cd",
+    "url": "/static/js/main.25251b4c.chunk.js"
+  },
+  {
+    "revision": "106938c293c8d0b6f0d6",
+    "url": "/static/js/runtime-main.21b8c31f.js"
+  }
+]);
