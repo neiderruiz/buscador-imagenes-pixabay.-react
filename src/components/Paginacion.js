9542dbf1e2a6cb85ba37392d75b86e1944f14405
neiderbtc <neiderbtc@gmail.com>
2021-12-10 09:16:53 -0500

    add desestructure props

--- src/components/Paginacion.js
+++ src/components/Paginacion.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from "react";
 
-const Paginacion = (props)=> {
-    return (
-        <div className="py-3">
-            <button type="button" onClick={props.paginaAnterior} className="btn btn-info mr-1">Anterior &larr;</button>
-            <button type="button" onClick={props.paginaSiguiente} className="btn btn-info mr-1">Siguiente &rarr;</button>
-        </div>
-    )
-}
+const Paginacion = ({ paginaAnterior, paginaSiguiente }) => {
+  const [before, setBefore] = React.useState(false);
+  return (
+    <div className="py-3">
+      <button
+        type="button"
+        disabled={before ? false : true}
+        onClick={() => setBefore(paginaAnterior())}
+        className="btn btn-info mr-1"
+      >
+        &larr; Anterior
+      </button>
+      <button
+        type="button"
+        onClick={() => setBefore(paginaSiguiente())}
+        className="btn btn-info mr-1"
+      >
+        Siguiente &rarr;
+      </button>
+    </div>
+  );
+};
 
-export default Paginacion
+export default Paginacion;
